4557d811473c036871880095b4b6a8c146487f2b
Jarom Plewe <jplewe23@gmail.com>
2020-08-13 10:31:16 -0600

    1-when questions hit 0, quiz will end. 2-When user makes a choice, another choice cannot be made.

--- Assets/javascript/javascript.js
+++ Assets/javascript/javascript.js
@@ -1,9 +1,10 @@
 // links to the DOM
 let question = document.getElementById('question');
 let answers = document.getElementsByClassName('multiple-choice');
+let nextQuestionBtn = document.getElementById('next-question-btn');
 
 // set an object array for questions and answers
-const questionsAndAnswers = [
+var questionsAndAnswers = [
     {
         "question": "question 1",
         "correctAnswer": "answer 1-1 - correct!",
@@ -76,20 +77,24 @@ const questionsAndAnswers = [
         "answer4": "answer 4",
     }
 ];
+var questionNumber;
 
 // start the quiz
-function createQuestions() {
-    // pull a random number from the questionsAndAnswers array and display the according question
-    let questionNumber = Math.floor(Math.random() * questionsAndAnswers.length);
+function createQuestions(questionsArray) {
 
-    question.textContent = questionsAndAnswers[questionNumber].question;
+    console.log(questionsArray.length)
+
+    // pull a random number from the questionsArray array and display the according question
+    let questionNumber = Math.floor(Math.random() * questionsArray.length);
+
+    question.textContent = questionsArray[questionNumber].question;
 
     // set the answers to an array
     let answerArr = [
-        questionsAndAnswers[questionNumber].correctAnswer,
-        questionsAndAnswers[questionNumber].answer2,
-        questionsAndAnswers[questionNumber].answer3,
-        questionsAndAnswers[questionNumber].answer4
+        questionsArray[questionNumber].correctAnswer,
+        questionsArray[questionNumber].answer2,
+        questionsArray[questionNumber].answer3,
+        questionsArray[questionNumber].answer4
     ]
 
     // shuffle the question order using the fisher-yates alg.
@@ -116,25 +121,51 @@ function createQuestions() {
 
     // if correct, display correct. 
     // else, display incorrect
+    let answered = false;
+
+    
     for (let i = 0; i < answers.length; i++) {
         answers[i].addEventListener('click', checkAnswer)
-
+        
         function checkAnswer() {
-            if (answers[i].textContent == questionsAndAnswers[questionNumber].correctAnswer) {
-                answers[i].classList.add("btn-outline-success");
-                answers[i].textContent = "Correct! " + answers[i].textContent;
-            } else {
-                answers[i].classList.add("btn-outline-danger");
-                answers[i].textContent = "Incorrect " + answers[i].textContent;
+            if (answered === false) {
+                if (answers[i].textContent == questionsArray[questionNumber].correctAnswer) {
+                    answers[i].classList.add("btn-outline-success");
+                    answers[i].textContent = "Correct! " + answers[i].textContent;
+                } else {
+                    answers[i].classList.add("btn-outline-danger");
+                    answers[i].textContent = "Incorrect " + answers[i].textContent;
+                }
             }
+            answered = true;
         };
     }
 
+    // record score to local storage
+
+
     // once an answer is clicked, you can't change your answer
 
-    // after question is answered and 'Next Question' button is clicked, remove item from questionsAndAnswers array
+    // after question is answered and 'Next Question' button is clicked, remove item from questionsArray array
+    console.log(questionsArray)
+    return;
+    
+}
 
+nextQuestionBtn.addEventListener('click', removeArr)
+function removeArr() {
+    questionsAndAnswers.splice((questionNumber - 1), 1)
+
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].classList.remove("btn-outline-success");
+        answers[i].classList.remove("btn-outline-danger");
+    }
+    answered = false;
+    createQuestions(questionsAndAnswers);
 }
 
 
-createQuestions();
+createQuestions(questionsAndAnswers);
+
+// nextQuestionBtn.addEventListener('click', createQuestions(questionsAndAnswers));
+
